ebd1f338dc78ad0bee56b2d3581b401edcfcb1ab
zeger015 <zeger_hoogeboom@hotmail.com>
2014-08-13 08:14:11 +0200

    Test setup

--- DomainManagerTest.java
+++ DomainManagerTest.java
@@ -13,24 +13,26 @@ public class DomainManagerTest extends TestCase {
 	@Rule
 	private ExpectedException exception = ExpectedException.none();
 
+	private IDomainManager manager;
+	
+	protected void setUp() throws Exception {
+		manager = new DomainManager();
+	}
+	
 	@Test
 	public void testGotUserInterface() {
-		IDomainManager manager = new DomainManager();
 		IUser user = manager.getEntity(User.class);
 		assertTrue(user instanceof IUser);
 	}
 	
 	@Test
 	public void testUserReturnsCorrectMessage() {
-		IDomainManager manager = new DomainManager();
 		IUser user = manager.getEntity(User.class);
 		assertEquals("Hi!", user.sayHi());
 	}
 	
 	@Test
 	public void testGetEntityWithoutDefaultConstructorThrowsException() {
-		IDomainManager manager = new DomainManager();
-		
 		exception.expect(InstantiationException.class);
 		Product product = manager.getEntity(Product.class);
 		assertTrue(product == null);
